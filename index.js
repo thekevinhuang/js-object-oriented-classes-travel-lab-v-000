7d182bf7ad7306003f2dfd155e4a9d272b66806a
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 16:45:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ class Driver {
 }
 
 class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
   blocksTravelled() {
     
   }
