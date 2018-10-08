ce70abf3ea9047369f49f08cad7d232ac8099f02
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 16:41:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = new Date(startDate)
   }
 
-  yearsExperienceFromBeginningOf(year){
+  yearsExperienceFromBeginningOf(year) {
     return year - this.startDate.getFullYear
   }
 }
