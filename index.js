a525e56ea2568e6b65a81608298a33b0e4207cc4
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 16:55:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor (name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+    this.eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  }
+
+  blocksTravelled() {
+    let totalBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
+    totalBlocks += Math.abs(this.eastWest.indexOf(this.endingLocation.horizontal) - this.eastWest.indexOf(this.beginningLocation.horizontal))
+    return totalBlocks
+  }
+
+  estimatedTime(peak) {
+    let blocks = this.blocksTravelled()
+    if (peak) {
+      return blocks/2
+    }
+    else {
+      return blocks/3
+    }
+  }
+}
