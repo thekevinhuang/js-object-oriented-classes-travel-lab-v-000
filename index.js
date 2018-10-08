9031ce1db0834f0c15398ebd746d7e2ce9b1d683
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 16:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ class Route {
     this.endingLocation = endingLocation
     const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   }
-  
+
   blocksTravelled() {
-    let totalBlocks = Math.abs(endingLocation.vertical - beginningLocation.vertical)
-    totalBlocks += Math.abs(eastWest.indexOf(endingLocation.horizontal) - eastWest.indexOf(beginningLocation.horizontal))
+    let totalBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
+    totalBlocks += Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal))
     return totalBlocks
   }
 
